b218686d859a7de3308e4db38860dc1bbae22e74
helloworldlethavefun <bluefloyd12321@gmail.com>
2024-10-13 13:56:38 +1300

    fixing dark mode still again

--- static/js/extra.js
+++ static/js/extra.js
@@ -5,19 +5,11 @@
 function toggledark() {
 	if (document.body.className == 'dark-mode') {
 		document.body.className = "";
-<<<<<<< HEAD
 		localStorage.setItem('darkmode', true);
-=======
-		localStorage.setItem('darkmode', False);
->>>>>>> 3b4e6c9b91193852ded3e985588e3da264054e72
 	}
 
 	else {
 		document.body.className = "dark-mode";
-<<<<<<< HEAD
 		localStorage.setItem('darkmode', true);
-=======
-		localStorage.setItem('darkmode', True);
->>>>>>> 3b4e6c9b91193852ded3e985588e3da264054e72
 	}
 }
