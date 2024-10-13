d0f69b3a0c3b83bd2739fc8a2057ff93eaf1878b
helloworldlethavefun <bluefloyd12321@gmail.com>
2024-10-13 13:55:00 +1300

    dark mode stuff

--- static/js/extra.js
+++ static/js/extra.js
@@ -5,11 +5,11 @@
 function toggledark() {
 	if (document.body.className == 'dark-mode') {
 		document.body.className = "";
-		localStorage.setItem('darkmode', False)
+		localStorage.setItem('darkmode', true);
 	}
 
 	else {
 		document.body.className = "dark-mode";
-		localStorage.setItem('darkmode', True)
+		localStorage.setItem('darkmode', true);
 	}
 }
