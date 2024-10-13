64285d63080daeeffb2a1bca87aa47df6e182ba7
helloworldlethavefun <bluefloyd12321@gmail.com>
2024-10-13 13:52:10 +1300

    dark mode stuff

--- static/js/extra.js
+++ static/js/extra.js
@@ -1,11 +1,15 @@
 // extra javascript shit I don't wanna cram into the main file
 
+
+
 function toggledark() {
 	if (document.body.className == 'dark-mode') {
 		document.body.className = "";
+		localStorage.setItem('darkmode', False)
 	}
 
 	else {
 		document.body.className = "dark-mode";
+		localStorage.setItem('darkmode', True)
 	}
 }
